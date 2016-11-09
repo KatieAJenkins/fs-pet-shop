b24c77cd2166f7244d200195a22afde0a18eb10f
KatieAJenkins <kind2karma@gmail.com>
2016-11-09 08:21:34 -0700

    created read functionality

--- pets.js
+++ pets.js
@@ -5,12 +5,22 @@ var path = require('path');
 var petsPath = path.join(__dirname, 'pets.json');
 // console.log(__dirname);
 
+var node = path.basename(process.argv[0]);
+var file = path.basename(process.argv[1]);
+var cmd = process.argv[2];
+
+if (cmd === 'read'){
 fs.readFile(petsPath, 'utf8', function (err, data) {
   if (err) {
     throw err;
   }
-
   var pets = JSON.parse(data);
 
   console.log(pets);
-});
+  });
+}
+else {
+  console.error(`Usage: ${node} ${file} read`);
+  process.exit(1);
+
+};
