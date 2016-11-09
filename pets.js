a3d0c134c61379ee03d3071c020f6ce413ec2641
KatieAJenkins <kind2karma@gmail.com>
2016-11-09 16:12:45 -0700

    added index functionality

--- pets.js
+++ pets.js
@@ -5,27 +5,33 @@ var path = require('path');
 var petsPath = path.join(__dirname, 'pets.json');
 // console.log(__dirname);
 
-var node = console.log(path.basename(process.argv[0]));
-
+var node = path.basename(process.argv[0]);
+// console.log(node);
 var file = path.basename(process.argv[1]);
-console.log(file);
+// console.log(file);
 var cmd = process.argv[2];
-console.log(cmd);
+// console.log(cmd);
 var index = process.argv[3];
-console.log(index);
+// console.log(index);
 
 if (cmd === 'read'){
 fs.readFile(petsPath, 'utf8', function (err, data) {
   if (err) {
     throw err;
   }
-  // var pets = data[1];
+  //logs entire pet object
   var pets = JSON.parse(data);
+  console.log(pets);
+
+ if (index < 0 || index > pets.length - 1) {
+    console.error(`Usage: ${node} ${file} read INDEX`);
+    process.exit(1);
+  }
+    //logs index of pets
   console.log(pets[index]);
-  });
+});
 }
 else {
   console.error(`Usage: ${node} ${file} read`);
   process.exit(1);
-
-};
+}
