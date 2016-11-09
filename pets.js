a0bce53f2a817dedbc3b33ac7612c4ba902cbeb5
KatieAJenkins <kind2karma@gmail.com>
2016-11-09 09:20:43 -0700

    console log pet index

--- pets.js
+++ pets.js
@@ -5,18 +5,23 @@ var path = require('path');
 var petsPath = path.join(__dirname, 'pets.json');
 // console.log(__dirname);
 
-var node = path.basename(process.argv[0]);
+var node = console.log(path.basename(process.argv[0]));
+
 var file = path.basename(process.argv[1]);
+console.log(file);
 var cmd = process.argv[2];
+console.log(cmd);
+var index = process.argv[3];
+console.log(index);
 
 if (cmd === 'read'){
 fs.readFile(petsPath, 'utf8', function (err, data) {
   if (err) {
     throw err;
   }
+  // var pets = data[1];
   var pets = JSON.parse(data);
-
-  console.log(pets);
+  console.log(pets[index]);
   });
 }
 else {
