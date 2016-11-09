ecdf4fb6861c34676b41b33be236cd5ccf686e59
KatieAJenkins <kind2karma@gmail.com>
2016-11-09 08:15:23 -0700

    add the basic scaffold

---
+++ pets.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var petsPath = path.join(__dirname, 'pets.json');
+// console.log(__dirname);
+
+fs.readFile(petsPath, 'utf8', function (err, data) {
+  if (err) {
+    throw err;
+  }
+
+  var pets = JSON.parse(data);
+
+  console.log(pets);
+});
